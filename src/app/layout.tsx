6bda3741f5eedbf182517597ba9a2a3378efc075
lausafin <laux0523@gmail.com>
2025-11-18 00:38:11 +0100

    refactor: Clean up globals.css and update layout.tsx for favicon integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="da">
       <head>
-        {/* 2. Tilføj scriptet her i <head>. 
-            'beforeInteractive' sikrer, at scriptet er klar, før brugeren kan interagere med siden. */}
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon"></link>
         <Script
           src="https://checkout.vipps.no/checkout-button/v1/vipps-checkout-button.js"
           strategy="beforeInteractive"
